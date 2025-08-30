0057789c3227985803089bb8df4e0b179755cc00
tishmalo <tishmalo99@gmail.com>
2025-08-30 20:01:04 +0300

    leetcode(easy): solved the longest common prefix problem

--- Easy/longestCommonPrefix.py
+++ Easy/longestCommonPrefix.py
@@ -6,7 +6,6 @@ class Solution:
         res=""
 
         for i in  range(len(str[0])):
-
             for s in str:
                 if i==len(s) or s[i]!=str[0][i]:
                     return res
